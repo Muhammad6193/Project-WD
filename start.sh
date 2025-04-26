0dbbe4bbe7425b0c8171bac4486cc33113cbc21c
Muhammad6193 <130302490+Muhammad6193@users.noreply.github.com>
2025-04-26 17:39:58 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-apt-get update
-apt-get install -y wget unzip
+# Installer Chrome pour undetected_chromedriver
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install -y ./google-chrome-stable_current_amd64.deb
-pip install -r requirements.txt
+
+# Démarrer ton bot
 python bot.py
+

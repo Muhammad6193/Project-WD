8e1549a0cbb1038c22502b84790d9a146c05f400
Muhammad6193 <130302490+Muhammad6193@users.noreply.github.com>
2025-04-26 18:56:06 +0200

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Installer Google Chrome
-apt update && apt install -y wget unzip
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install -y ./google-chrome-stable_current_amd64.deb
-
-# Lancer le bot
-python bot.py
